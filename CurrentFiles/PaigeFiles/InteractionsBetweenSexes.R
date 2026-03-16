512f816480eab133a2f4a1cb621ea865438ba6a6
pgrantz-trinity <pgrantz@trinity.edu>
2026-03-16 10:01:41 -0500

    Finished comparing captured locations to rfid locations

--- CurrentFiles/PaigeFiles/InteractionsBetweenSexes.R
+++ CurrentFiles/PaigeFiles/InteractionsBetweenSexes.R
@@ -3,16 +3,16 @@
 #Author: Paige Grantz
 
 #Reads in the Pruned RFID data sheet.
-rfidData <- read.csv("CurrentFiles/PrunedRFID_Data_With_MBS.csv")
+rfidData <- read.csv("CurrentFiles/PaigeFiles/MergedRfidData.csv")
 
 #Reads in the master banding data sheet.
-masterBandingData <- read.csv("CurrentFiles/MasterBandingDataBracken.csv")
+masterBandingData <- read.csv("CurrentFiles/PaigeFiles/MasterBandingData23Jan2026.csv")
 
 #Prune the master banding data to get the most recent info.
 prunedMBD <- masterBandingData[!duplicated(masterBandingData$RFID, fromLast = TRUE),]
 
 #Merge the prunedMBD with the rfidData to have bird color Combo and sexes.
-combinedInfo <- merge(rfidData, prunedMBD, by.x="RFID",by.y="RFID..")[, c("RFID","DateTime","Feeder","GapToPrevious","Color.Combo","Sex")]
+combinedInfo <- merge(rfidData[, c("RFID","DateTime","Feeder","GapToPrevious","ColorCombo","Sex")], prunedMBD, by.x="RFID",by.y="RFID")
 
 #From the list of RFID data, gets the vector of unique individuals that visit the feeder.
 vectorOfIndividuals <- unique(rfidData$RFID)
@@ -21,11 +21,11 @@ vectorOfIndividuals <- unique(rfidData$RFID)
 tableofIndividuals <- data.frame(RFID=vectorOfIndividuals)
 
 #Now to add the individual's sex to the list of individuals.
-IndividualsAndSex <- merge(tableofIndividuals, prunedMBD, by.x="RFID",by.y="RFID..")[, c("RFID","Sex")]
+IndividualsAndSex <- merge(tableofIndividuals, prunedMBD, by.x="RFID",by.y="RFID")[, c("RFID","Sex")]
 
 #Separate out the sex of individuals, using filter() from the dplyr library (Make sure you have that)
 #Mid fall 2025 info for the comments
-maleIndividuals <- filter(IndividualsAndSex, Sex=="M") # As of writing, this has 42 observations of 70, 60%
+maleIndividuals <- filter(IndividualsAndSex, Sex=="M"  ) # As of writing, this has 42 observations of 70, 60%
 femaleIndividuals <- filter(IndividualsAndSex, Sex=="F") # 10 observations, 14.29% of population
 unknownIndividuals <- filter(IndividualsAndSex, Sex=="") # 18 observations, 25.71%
 
